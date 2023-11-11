f72b9e2e421c79ee16556be51264fff1f3e9488f
Practice test <bonniebraer@gmail.com>
2023-11-11 19:59:48 +0300

    Return int2

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,9 +2,9 @@ public class BmiService {
     public int calculate(double weightKg, double heightM) {
         if (weightKg < 0 || heightM < 0) {
             weightKg = 0;
-        } ;
-
-            return (int) (weightKg / (heightM*heightM));
         }
+
+        return (int) (weightKg / (heightM * heightM));
     }
+}
 
